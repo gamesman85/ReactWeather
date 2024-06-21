2b606cdfa270a3874663e7d33a34647d64548574
Daniel Wang <daniel.wang@try.no>
2024-06-21 16:16:35 +0200

    WIP

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const WeatherDisplay = ({ data }) => {
+  const weather = data.main;
+
   return (
     <div>
       <h2>Current Weather</h2>
-      <p>Temperature: {data.main.temp}°C</p>
+      <p>Temperature: {weather.temp}°C</p>
+      <p>Feels like: {weather.feels_like}°C</p>
+      <p>Minimum Temperature: {weather.temp_min}°C</p>
+      <p>Maximum Temperature: {weather.temp_max}°C</p>
       <p>Humidity: {data.main.humidity}%</p>
       <p>Condition: {data.weather[0].description}</p>
     </div>
